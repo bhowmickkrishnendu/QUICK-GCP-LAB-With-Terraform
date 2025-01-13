145036bbc870c001e245e80900865be24e1cd1b2
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-01-13 18:36:40 +0530

    variable file added

---
+++ Build a Website on Google Cloud (GSP319)/gcp-gke-microservices-deployment/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {}
+variable "region" {
+  default = "us-central1"
+}
+variable "zone" {
+  default = "us-central1-a"
+}
+variable "cluster_name" {
+  default = "monolith-cluster"
+}
+variable "monolith_name" {
+  default = "monolith"
+}
+variable "orders_name" {
+  default = "orders"
+}
+variable "products_name" {
+  default = "products"
+}
+variable "frontend_name" {
+  default = "frontend"
+}
